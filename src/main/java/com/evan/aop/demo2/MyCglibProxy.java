aab4e5f52396fbfaf4790c261344a0bab49418d9
EvanZhao <phzhao7548@gmail.com>
2019-06-20 22:25:45 +0800

    Cglib方式代理

---
+++ src/main/java/com/evan/aop/demo2/MyCglibProxy.java
@@ -0,0 +1,37 @@
+package com.evan.aop.demo2;
+
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class MyCglibProxy implements MethodInterceptor {
+
+    private  ProductDao productDao;
+
+    public MyCglibProxy(ProductDao productDao){
+        this.productDao = productDao;
+
+    }
+
+    public Object createProxy(){
+        //创建核心类
+        Enhancer enhancer = new Enhancer();
+        //设置父类
+        enhancer.setSuperclass(productDao.getClass());
+        //设置回调
+        enhancer.setCallback(this);
+        //生成代理
+        Object proxy = enhancer.create();
+        return proxy;
+    }
+
+    @Override
+    public Object intercept(Object proxy, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+        if("save".equals(method.getName())){
+            System.out.println("权限校验");
+        }
+        return methodProxy.invokeSuper(proxy,args);
+    }
+}
---
+++ src/main/java/com/evan/aop/demo2/ProductDao.java
@@ -0,0 +1,20 @@
+package com.evan.aop.demo2;
+
+public class ProductDao {
+
+    public void save(){
+        System.out.println("保存商品");
+    }
+
+    public void update(){
+        System.out.println("更新商品");
+    }
+
+    public void delete(){
+        System.out.println("删除商品");
+    }
+
+    public void find(){
+        System.out.println("查找商品");
+    }
+}
