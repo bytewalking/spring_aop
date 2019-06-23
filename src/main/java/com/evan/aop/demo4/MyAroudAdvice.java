f1a6f7b7228a210cee3235d1491d5ba85812adb7
EvanZhao <phzhao7548@gmail.com>
2019-06-23 23:44:23 +0800

    带入切入点的切面的配置

---
+++ src/main/java/com/evan/aop/demo4/MyAroudAdvice.java
@@ -0,0 +1,19 @@
+package com.evan.aop.demo4;
+
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+public class MyAroudAdvice implements MethodInterceptor {
+
+    @Override
+    public Object invoke(MethodInvocation methodInvocation) throws Throwable {
+
+        System.out.println("环绕前增强==========");
+
+        Object obj = methodInvocation.proceed();
+
+        System.out.println("环绕后增强==========");
+        return obj;
+    }
+}
