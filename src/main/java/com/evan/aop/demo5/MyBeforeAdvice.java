2f7a34b251a7a88cd4fab03de73aa430fb24ab9f
EvanZhao <phzhao7548@gmail.com>
2019-06-24 00:30:29 +0800

    基于bean名称的自动代理

---
+++ src/main/java/com/evan/aop/demo5/MyBeforeAdvice.java
@@ -0,0 +1,12 @@
+package com.evan.aop.demo5;
+
+import org.springframework.aop.MethodBeforeAdvice;
+
+import java.lang.reflect.Method;
+
+public class MyBeforeAdvice implements MethodBeforeAdvice {
+    @Override
+    public void before(Method method, Object[] objects, Object o) throws Throwable {
+        System.out.println("前置通知类型...");
+    }
+}
